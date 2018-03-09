9e5ce46eab04ed86d23e2493c894e61160b5c256
OffensivelyBad <OffensivelyBad@yahoo.com>
2018-03-08 22:35:54 -0500

    Adds collisions and music

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class Player : MonoBehaviour {
 
+	[SerializeField] private AudioClip sfxJump;
+	[SerializeField] private AudioClip sfxDeath;
 	[SerializeField] private float jumpForce = 120f;
 	private Animator anim;
 	private Rigidbody rigidBody;
 	private bool jump;
+	private AudioSource audioSource;
+
+	void Awake() {
+		Assert.IsNotNull (sfxJump);
+		Assert.IsNotNull (sfxDeath);
+	}
 
 	// Use this for initialization
 	void Start () {
 		anim = GetComponent<Animator> ();
 		rigidBody = GetComponent<Rigidbody> ();
+		audioSource = GetComponent<AudioSource> ();
 	}
 	
 	// Update is called once per frame
@@ -20,6 +30,7 @@ public class Player : MonoBehaviour {
 
 		if (Input.GetMouseButtonDown (0)) {
 			anim.Play ("Jump");
+			audioSource.PlayOneShot (sfxJump);
 			rigidBody.useGravity = true;
 			jump = true;
 		}
@@ -34,6 +45,14 @@ public class Player : MonoBehaviour {
 			rigidBody.AddForce (new Vector2 (0, jumpForce), ForceMode.Impulse);
 		}
 
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		if (collision.gameObject.tag == "obstacle") {
+			rigidBody.AddForce (new Vector2 (-75, 35), ForceMode.Impulse);
+			rigidBody.detectCollisions = false;
+			audioSource.PlayOneShot (sfxDeath);
+		}
 	}
 
 }
